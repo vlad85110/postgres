5a6222c5b76d67c8f65c9630a71b2779cb3a49f7
Borus25 <maksimborisov51046@gmail.com>
2026-08-04 11:46:39 +0700

    создал stand_client.py - отправляет запросы через .sql файл, замеряет время и оборачивает его в JSONL

--- contrib/rest_bgworker/rest_bgworker.c
+++ contrib/rest_bgworker/rest_bgworker.c
@@ -11,47 +11,10 @@
 
 PG_MODULE_MAGIC;
 
-static int my_initial_value = 10;
-
-int *shared_value = NULL;
-
 static bool worker_registered = false;
 
-PG_FUNCTION_INFO_V1(print_hello_c);
-PG_FUNCTION_INFO_V1(set_value);
-
-Datum
-print_hello_c(PG_FUNCTION_ARGS)
-{
-    char *message = "hello from c";
-    text *result = cstring_to_text(message);
-    PG_RETURN_TEXT_P(result);
-}
-
-Datum
-set_value(PG_FUNCTION_ARGS)
-{
-    int new_value = PG_GETARG_INT32(0);
-
-    if (shared_value == NULL)
-    {
-        bool found;
-
-        shared_value = (int *) ShmemInitStruct("my_extension_shared", sizeof(int), &found);
-        
-        if (!found)
-        {
-            *shared_value = my_initial_value;
-            elog(LOG, "my_extension: shared memory initialized with value %d", my_initial_value);
-        }
-    }
-    *shared_value = new_value;
-    elog(LOG, "my_extension: value changed to %d", new_value);
-    PG_RETURN_VOID();
-}
-
 PGDLLEXPORT void
-my_bgworker_main(Datum main_arg)
+RestBgworker_main(Datum main_arg)
 {
 
     bool found;
