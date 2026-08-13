c8b04b6c798b1f540e0350ee77c6d5b342e3b8c6
Victoria <yurganova05@gmail.com>
2026-08-14 01:08:04 +0700

    встроила в autovacuum

--- src/backend/postmaster/autovacuum.c
+++ src/backend/postmaster/autovacuum.c
@@ -110,6 +110,8 @@
 #include "utils/syscache.h"
 #include "utils/timeout.h"
 #include "utils/timestamp.h"
+#include "rest/endpoint_handlers.h"
+#include "rest/rest_server.h"
 
 
 /*
@@ -597,6 +599,8 @@ AutoVacLauncherMain(const void *startup_data, size_t startup_data_len)
 		ResetLatch(MyLatch);
 
 		ProcessAutoVacLauncherInterrupts();
+
+		rest_server_poll();
 
 		/*
 		 * a worker finished, or postmaster signaled failure to start a worker
